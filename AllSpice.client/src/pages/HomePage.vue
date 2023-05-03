4da8b1e9ef27024a2f5281cc4dc1c0498a20708d
LemonadeGT1 <michael.p.patience@gmail.com>
2023-05-02 22:50:05 -0600

    Nav & 508 updates

--- AllSpice.client/src/pages/HomePage.vue
+++ AllSpice.client/src/pages/HomePage.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mb-2">
       <div class="col-md-4 my-2 order-md-0 order-1">
-        <span class="text-small text-secondary">&OpenCurlyQuote;Cook up something special with our
-          recipes.&CloseCurlyQuote;</span>
+        <span class="text-small text-secondary">Cook up something special with our
+          recipes.</span>
       </div>
-      <div class="col-md-4 text-center order-md-1 order-0 text-small text-secondary">
+      <div
+        class="col-md-4 text-center order-md-1 order-0 text-small text-secondary elevation-3 bg-light rounded pb-1 mb-0 pt-1">
 
         <div class="row">
           <div class="col-4"><span class="selectable rounded px-2 pb-1">All Recipes</span></div>
@@ -14,12 +15,16 @@
         </div>
 
       </div>
+
+      <div class="col-md-4 my-2 order-md-3 order-3 text-end">
+        <span class="text-small text-secondary">...or just do take-out.</span>
+      </div>
     </div>
 
     <!-- SECTION - Recipe Card -->
     <div class="row">
       <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-2" v-for="r in recipes" :key="r.id">
-        <div class="imgContainer pt-1 elevation-3" :style="{ 'background-image': 'url(' + r.img + ')' }" :title="r.title">
+        <div class="imgContainer pt-1 elevation-3" :style="{ 'background-image': 'url(' + r.img + ')' }">
 
           <div class="row justify-content-between">
             <div class="col-9">
@@ -37,7 +42,8 @@
           <div class="row setHeight">
             <div class="col-12 titleContainer">
               <div :class="r.creatorId != account.id ? 'recipeGlass m-2 px-3' : 'myRecipeGlass m-2 px-3'">
-                <h6>{{ r.title.substring(0, 25) }} {{ r.title[26] ? '...' : '' }}</h6>
+                <h6 :title="r.creatorId != account.id ? r.title : 'Your recipe for: ' + r.title">{{ r.title.substring(0,
+                  25) }} {{ r.title[26] ? '...' : '' }}</h6>
               </div>
             </div>
           </div>
@@ -111,11 +117,11 @@ export default {
 
 .myRecipeGlass {
   /* From https://css.glass */
-  background: rgba(67, 29, 238, 0.5);
+  background: rgba(195, 160, 64, 0.5);
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
   border-radius: 500px;
-  color: white;
+  // color: white;
 }
 
 .imgContainer {
