d84470a04fd7eac30774a1fea0df1ccdaf52d947
LemonadeGT1 <michael.p.patience@gmail.com>
2023-05-02 19:42:28 -0600

    mobile view updates

--- AllSpice.client/src/pages/HomePage.vue
+++ AllSpice.client/src/pages/HomePage.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12">
-        <h2 class="text-secondary">Here is the Home Page.</h2>
+      <div class="col-md-4 my-2 order-md-0 order-1">
+        <span class="text-small text-secondary">"Cook up something special with our recipes."</span>
+      </div>
+      <div class="col-md-4 text-center order-md-1 order-0 text-small text-secondary">
+        <span>Home</span>
+        <span>My Recipes</span>
+        <span>Favorites</span>
       </div>
     </div>
     <div class="row">
-      <div class="col-12 col-md-4 my-2" v-for="r in recipes" :key="r.id">
+      <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-2" v-for="r in recipes" :key="r.id">
         <div class="imgContainer pt-1" :style="{ 'background-image': 'url(' + r.img + ')' }">
 
           <div class="row justify-content-between">
-            <div class="col-4">
+            <div class="col-6">
               <div class="recipeGlass text-center m-2">
                 <h5 class="text-dark">{{ r.category }}</h5>
               </div>
             </div>
-            <div class="col-2">
+            <div class="col-3">
               <div class="recipeGlass text-center m-2 selectable">
                 <span class="mdi mdi-heart-outline"></span>
               </div>
             </div>
           </div>
 
-          <div class="row">
+          <div class="row setHeight">
             <div class="col-12 titleContainer">
               <div class="recipeGlass m-2 px-3">
-                <h5>{{ r.title }}</h5>
+                <h5>{{ r.title.substring(0, 22) + '...' }}</h5>
               </div>
             </div>
           </div>
@@ -84,10 +89,10 @@ export default {
 
 .recipeGlass {
   /* From https://css.glass */
-  background: rgba(217, 219, 214, 0.5);
+  background: rgba(234, 239, 227, 0.5);
   // box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(6px);
-  -webkit-backdrop-filter: blur(6px);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
   // color: #000000;
   // padding: 10px 10px 1px 10px;
   // height: 12vh;
@@ -104,7 +109,12 @@ export default {
 }
 
 .titleContainer {
-  height: 20vh;
+  // height: 20vh;
+  margin-top: 15vh;
   width: auto;
+}
+
+.setHeight {
+  height: 20vh;
 }
 </style>
